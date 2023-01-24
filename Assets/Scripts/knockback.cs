6be8f504e76b83542eb4e35527976797502c653d
MervinA <mervinarmoogan21@gmail.com>
2023-01-24 01:46:13 -0400

    added directional walk animations for enemy

--- Assets/Scripts/knockback.cs
+++ Assets/Scripts/knockback.cs
@@ -10,7 +10,7 @@ public class knockback : MonoBehaviour
 
 private void OnTriggerEnter2D (Collider2D other){
 
- if(other.gameObject.CompareTag("breakable")){
+ if(other.gameObject.CompareTag("breakable") && this.gameObject.CompareTag("Player")){
 
             other.GetComponent<pot>().Smash();
         }
