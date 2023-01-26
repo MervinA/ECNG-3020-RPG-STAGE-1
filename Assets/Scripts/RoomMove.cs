997bacefeed634885e5c11fc144dbfcae636dc84
MervinA <mervinarmoogan21@gmail.com>
2023-01-26 11:50:02 -0400

    reworking tilemap 2)

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -36,7 +36,7 @@ public class RoomMove : MonoBehaviour
 
    private void OnTriggerEnter2D(Collider2D other){
 
-    if(other.CompareTag("Player") && other.isTrigger){
+    if(other.CompareTag("Player") && !other.isTrigger){
 
             cam.minPosition = cameraChange_min; 
             cam.maxPosition = cameraChange_max; 
